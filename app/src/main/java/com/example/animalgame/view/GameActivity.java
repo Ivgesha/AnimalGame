e18e2fc60e4ff77db6da084185ac25bfca8816f3
Ivgesha <evgenise93@gmail.com>
2020-01-13 14:51:19 -0800

    added icon for app

---
+++ app/src/main/java/com/example/animalgame/view/GameActivity.java
@@ -0,0 +1,45 @@
+package com.example.animalgame.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ContentLoadingProgressBar;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.animalgame.R;
+
+import org.w3c.dom.Text;
+
+public class GameActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView countDownTimerTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        progressBar = findViewById(R.id.progressBar);
+        countDownTimerTextView = findViewById(R.id.countDownTimerTextView);
+        countDownTimerTextView.setText("Timer Time");
+
+        new CountDownTimer(10000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                progressBar.setProgress((int) millisUntilFinished / 1000);
+                countDownTimerTextView.setText("Timer: "+progressBar.getProgress());               // is there a better way?
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(GameActivity.this, "DONE!", Toast.LENGTH_SHORT).show();
+            }
+        }.start();
+
+    }
+}
--- app/src/main/java/com/example/animalgame/view/MainActivity.java
+++ app/src/main/java/com/example/animalgame/view/MainActivity.java
@@ -2,17 +2,55 @@ package com.example.animalgame.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.animalgame.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Intent intent;
+
+    EditText enterNameEditText;
+    Button startGameButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        enterNameEditText= findViewById(R.id.EnterNameEditText);
+        startGameButton = findViewById(R.id.startGameButton);
+
+        // for testing only
+        enterNameEditText.setText("Test");
+
+
+    }
+
+   public void startGameButtonClick(View view){                 // on click startGame Button
+        intent = new Intent(this,GameActivity.class);
+       Log.d("enterNameEditText",enterNameEditText.getText().toString());
+        if(enterNameEditText.getText().toString().equals("")){
+            Toast.makeText(this,"name is empty",Toast.LENGTH_LONG).show();
+        }
+        else{
+            this.startActivity(intent);
+            Toast.makeText(this,"name is full! we can play",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void howToPlayClick(View view){                      // on click howToPlay Button
+        intent = new Intent(this,HowToPlayActivity.class);
+        startActivity(intent);
     }
 }
