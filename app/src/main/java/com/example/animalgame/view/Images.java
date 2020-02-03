8f37396700580340a78e33cfdb7e93de32ead5fb
Bovich <49761044+Bovich@users.noreply.github.com>
2020-02-03 13:23:08 +0200

    change photos on click and update the points earned

---
+++ app/src/main/java/com/example/animalgame/view/Images.java
@@ -0,0 +1,15 @@
+package com.example.animalgame.view;
+
+import java.util.Random;
+
+public class Images {
+    public String key;
+    public int img_path;
+
+    public Images(String key, int img_path) {
+        this.key = key;
+        this.img_path = img_path;
+    }
+
+
+}
